2669234bce74bc85799401982259f5c7c18f2475
Roy Joseph <royjoseph.mec@gmail.com>
2023-06-01 13:37:55 +0530

    Now the program is capeable of playing a single round of rock,paper,scissors

--- rock.js
+++ rock.js
@@ -1,13 +1,34 @@
 const getComputerChoice = () => {
     let choice=Math.floor(Math.random()*3)
-    if(choice == 0){
+    if(choice === 0){
         return "Rock"
-    }else if(choice == 1){
+    }else if(choice === 1){
         return "Paper"
     }else{
         return "Scissors"
     }
 }
 
-let computerChoice = getComputerChoice()
-console.log(computerChoice)
+const playRound = (computer,player) => {
+    if(computer.toUpperCase() === player.toUpperCase()){
+        return "Draw"
+    }else if(computer.toUpperCase() === "ROCK" && player.toUpperCase() == "PAPER"){
+        return "You Won! Paper beats Rock"
+    }else if(computer.toUpperCase() === "ROCK" && player.toUpperCase() == "SCISSORS"){
+        return "You Lost! Rock beats Scissor"
+    }else if(computer.toUpperCase() === "PAPER" && player.toUpperCase() == "ROCK"){
+        return "You Lost! Paper beats Rock"
+    }else if(computer.toUpperCase() === "PAPER" && player.toUpperCase() == "SCISSORS"){
+        return "You Won! Scissors beats Paper"
+    }else if(computer.toUpperCase() === "SCISSORS" && player.toUpperCase() == "ROCK"){
+        return "You Won! Rock beats Scissors"
+    }else if(computer.toUpperCase() === "SCISSORS" && player.toUpperCase() == "PAPER"){
+        return "You Lost! Scissors beats Paper"             
+    }
+}
+
+let computerSelection = getComputerChoice()
+let playerSelection = prompt("Enter your choice: Either Rock, Paper or Scissors")
+
+let result= playRound(computerSelection,playerSelection)
+console.log(result)
